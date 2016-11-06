f0f795c7746876b80c4750df3fbd76629a060cee
Simon West <simon.west@cantab.net>
2016-11-06 15:04:13 +0000

    get-multisales.py: for >1 sale on the same date, take the last one

--- scripts/get-multisales.py
+++ scripts/get-multisales.py
@@ -6,21 +6,31 @@ import sys
 n_cols_to_compare = 4
 previous_address = [None for i in xrange(n_cols_to_compare)]
 n_seen = 1
-addresses = []
+full_lines = []
 
 writer = csv.writer(sys.stdout, delimiter=',', quotechar='"')
 
 for tokens in csv.reader(sys.stdin, delimiter=','):
-    values = tokens[:n_cols_to_compare]
-    if values != previous_address:
+    address = tokens[:n_cols_to_compare]
+    if address != previous_address:
         if n_seen != 1:
             entries_to_write = previous_address
-            for address in addresses:
-                entries_to_write.extend(address[n_cols_to_compare:])
-            writer.writerow(entries_to_write)
-        addresses = [tokens]
+            entries_to_write.extend(full_lines[0][n_cols_to_compare:])
+            for full_line in full_lines[1:]:
+                date, price = full_line[n_cols_to_compare:]
+                # If >1 price on a single day, overwrite (i.e., take
+                # the last one only)
+                if date == entries_to_write[-2]:
+                    entries_to_write[-1] = price
+                else:
+                    entries_to_write.extend([date, price])
+            # Check that there were sales on > 1 date (repeated sales
+            # on a single date do not count)
+            if (len(entries_to_write) - n_cols_to_compare)/2 > 1:
+                writer.writerow(entries_to_write)
+        full_lines = [tokens]
         n_seen = 1
-        previous_address = values
+        previous_address = address
     else:
         n_seen += 1
-        addresses.append(tokens)
+        full_lines.append(tokens)
