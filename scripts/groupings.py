e967103d4fa2464971e9ea15046a5fa750672600
Simon West <simon.west@cantab.net>
2021-02-27 17:41:19 +0000

    allow postcode areas to be specified without adding extra code

--- scripts/groupings.py
+++ scripts/groupings.py
@@ -2,14 +2,9 @@
 def get_records_by_first_half_of_postcode(first_half_of_postcode, data):
     return data[data.POSTCODE.str.startswith(f"{first_half_of_postcode} ")]
 
-first_half_of_postcode_areas = {
-    "hampstead": "NW3",
-    "highgate": "N6"
-}
+def get_records(grouping, data):
 
-def get_records(grouping_name, data):
-
-    if grouping_name in first_half_of_postcode_areas:
-        return get_records_by_first_half_of_postcode(first_half_of_postcode_areas[grouping_name], data)
+    if grouping['type'] == 'first half of postcode':
+        return get_records_by_first_half_of_postcode(grouping['postcode'], data)
 
     raise Exception(f"unknown grouping {grouping_name}")
